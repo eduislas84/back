2a1e8dcdbbb5f29c2fbc6810c7e48379cdad6ffc
eduislas84 <122810400+eduislas84@users.noreply.github.com>
2023-12-13 10:00:32 -0600

    vxcb

---
+++ sql/contactos.sql
@@ -0,0 +1,17 @@
+CREATE TABLE contactos (
+    email varchar(100) PRIMARY KEY,
+    nombre varchar(50),
+    telefono varchar(12)
+);
+
+INSERT INTO contactos (email, nombre, telefono)
+VALUES ('juan@example.com', 'Juan Pérez', '555-123-4567');
+
+INSERT INTO contactos (email, nombre, telefono)
+VALUES ('maria@example.com', 'María García', '555-678-9012');
+
+CREATE TABLE IF NOT EXISTS usuarios(
+    username TEXT PRIMARY KEY,
+    password  TEXT NOT NULL,
+    token     TEXT NOT NULL
+);
